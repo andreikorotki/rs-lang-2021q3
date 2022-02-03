0a1dfffcc97d2446d7fea62783be16b8c35aa751
Andrei Karotki <Andreikorotki@gmail.com>
2022-02-03 12:05:25 +0300

    fix: correct tokenExpireDate in localStorage (UTC time)

--- src/components/controllers/user-controller.js
+++ src/components/controllers/user-controller.js
@@ -3,13 +3,14 @@ import { createUser, loginUser } from '../api/users';
 import User from '../models/user';
 import { updateState } from '../services/state';
 import { filterObject } from '../services/utils';
+import { tokenExpirationPeriodMs } from '../services/settings';
 
 export async function loginUserController(userEmail, userPassword) {
   let user = new User(userEmail);
   const responseData = await loginUser({ email: user.email, password: userPassword });
   if (responseData.success) {
     user = { ...user, ...responseData.content };
-    user.tokenExpireDate = new Date();
+    user.tokenExpireDate = new Date(Date.now() + tokenExpirationPeriodMs);
     updateState(user);
   }
   return responseData;
